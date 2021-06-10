2c943149cf23c2e8125230f2a43de146bfae775d
Claire Fields <clairefields15@gmail.com>
2021-06-09 21:18:18 -0600

    Add landing page to select page view on click

--- src/scripts.js
+++ src/scripts.js
@@ -9,14 +9,30 @@ import domUpdates from './domUpdates'
 
 // query selectors
 let hamburgerBtn = document.getElementById('hamburger');
-let dayjs = require('dayjs')
-
-
+let mobileBookBtn = document.getElementById('mobileBook');
+let bookNowBtn = document.getElementById('bookNowBtn');
+let mobileLogInBtn = document.getElementById('mobileLogIn');
+let mobileViewTripsBtn = document.getElementById('mobileViewTrips');
+let mobileViewProfileBtn = document.getElementById('mobileViewProfile');
+let navLogInBtn = document.getElementById('navLogIn');
+let navBookBtn = document.getElementById('navBook');
+let navTripsBtn = document.getElementById('navTrips');
+let navProfileBtn = document.getElementById('navProfile');
+let desktopNavSection = document.getElementById('desktopNav');
+let landingPage = document.getElementById('landingPage');
+let selectPage = document.getElementById('selectPage');
+let searchResultsPage = document.getElementById('searchResultsPage');
+let bookingPage = document.getElementById('bookingPage');
+let confirmationPage = document.getElementById('confirmationPage');
+  
 // variables
+let dayjs = require('dayjs')
 
 
 // event listeners
 hamburgerBtn.addEventListener('click', domUpdates.openMobileNav)
+mobileBookBtn.addEventListener('click', domUpdates.showBookingView)
+bookNowBtn.addEventListener('click', domUpdates.showBookingView);
 
 //event handlers and functions
 
