fa7c4956d3611dab2ea4ebfbc8b41bcd49cfacaa
Claire Fields <clairefields15@gmail.com>
2021-06-10 17:38:52 -0600

    Instantiate bookings on load

--- src/scripts.js
+++ src/scripts.js
@@ -60,7 +60,6 @@ export function assignVariables(data) {
   customersData = data[0].customers;
   roomsData = data[1].rooms;
   bookingsData = data[2].bookings;
-  console.log(bookingsData[0])
 }
 
 export function pageLoad() {
