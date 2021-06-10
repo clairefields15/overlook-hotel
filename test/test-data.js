d598f7f8a8b1b293299f5b9e16d24290993584fe
Claire Fields <clairefields15@gmail.com>
2021-06-10 17:57:06 -0600

    Edit test data so that userIDs of bookings reflect users in test data

--- test/test-data.js
+++ test/test-data.js
@@ -48,28 +48,28 @@ const rooms = [
 const bookings = [
   {
     "id": "5fwrgu4i7k55hl6sz",
-    "userID": 9,
+    "userID": 1,
     "date": "2020/04/22",
     "roomNumber": 15,
     "roomServiceCharges": []
   },
   {
     "id": "5fwrgu4i7k55hl6t5",
-    "userID": 43,
+    "userID": 2,
     "date": "2020/01/24",
     "roomNumber": 24,
     "roomServiceCharges": []
   },
   {
     "id": "5fwrgu4i7k55hl6t6",
-    "userID": 13,
+    "userID": 1,
     "date": "2020/01/10",
     "roomNumber": 12,
     "roomServiceCharges": []
   },
   {
     "id": "5fwrgu4i7k55hl6t7",
-    "userID": 20,
+    "userID": 2,
     "date": "2020/02/16",
     "roomNumber": 7,
     "roomServiceCharges": []
