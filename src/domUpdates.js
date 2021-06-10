ad50a20885f0177bf47af1dbb6a0483f81575dbc
Claire Fields <clairefields15@gmail.com>
2021-06-09 18:17:27 -0600

    Add hamburger menu

---
+++ src/domUpdates.js
@@ -0,0 +1,29 @@
+let domUpdates = {
+  hide(elements) {
+    elements.forEach(element => element.classList.add('hidden'));
+  },
+
+  show(elements) {
+    elements.forEach(element => element.classList.remove('hidden'));
+  },
+
+  openMobileNav() {
+    let hamburgerBtn = document.getElementById('hamburger');
+    let hamburgerImg = document.querySelector('.ham-img')
+    let closeButton = document.querySelector('.close-btn');
+    let attr = hamburgerBtn.getAttribute('aria-expanded');
+    let menuDropdown = document.getElementById('menuDropdown')
+
+    if (attr === 'false') {
+      hamburgerBtn.setAttribute('aria-expanded', true);
+      domUpdates.show([menuDropdown, closeButton]);
+      domUpdates.hide([hamburgerImg])
+    } else {
+      hamburgerBtn.setAttribute('aria-expanded', false);
+      domUpdates.hide([menuDropdown, closeButton]);
+      domUpdates.show([hamburgerImg])
+    }
+  }
+};
+
+export default domUpdates
--- src/scripts.js
+++ src/scripts.js
@@ -2,15 +2,19 @@
 import './css/index.scss';
 import './images/hotel-logo.png';
 import './images/hotel-image.jpg';
+import './images/hamburger.png';
+import './images/hamburger-close.png';
+import domUpdates from './domUpdates'
 
 // query selectors
+let hamburgerBtn = document.getElementById('hamburger');
 
 
 // variables
 
 
 // event listeners
-
+hamburgerBtn.addEventListener('click', domUpdates.openMobileNav)
 
 //event handlers and functions
 
