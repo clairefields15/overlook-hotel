74ce487b725d3a3c12d7b89a7ea87dab67ead215
Claire Fields <clairefields15@gmail.com>
2021-06-11 18:04:08 -0600

    Set up innerHTML to render search results

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -51,7 +51,6 @@ const domUpdates = {
       domUpdates.hide([menuDropdown, closeButton, modalOverlay]);
       domUpdates.show([hamburgerImg]);
     }
-  
   },
 
   hideOverlay() {
@@ -151,7 +150,7 @@ const domUpdates = {
     totalSpent.innerText = `Thanks for staying with us! You have spent $${expenses} on bookings.`;
   },
 
-  checkAvailability() {
+  showSearchResultsPage() {
     domUpdates.show([searchResultsPage]);
     domUpdates.hide([
       selectPage,
@@ -160,6 +159,29 @@ const domUpdates = {
       profilePage, 
       confirmationPage
     ]);
+  },
+
+  renderAvailableRooms(availableRooms) {
+    console.log('avail rooms DOM UPdates 165:', availableRooms)
+
+    // availableRooms.forEach(room => {
+    //   console.log(room)
+    //   searchResultsPage.innerHTML += `
+    //     <article class="room-card" id="${id}">
+    //       <div class="image-area">
+    //         <div class="image-container">
+    //           <img src="./images/1-bed-room.jpg" class="room-photo" alt="Light and airy room with double bed">
+    //           <button id="selectRoomButton" class="select-room-btn">Select this room</button>
+    //         </div>
+    //       </div>
+    //       <div class="text-area">
+    //         <h3 id="roomType" class="room-type">${type}</h3>
+    //         <p id="typeOfBed">${num beds} ${type bed}</p>
+    //         <p id="costPerNight">$${cost per night} per night</p>
+    //       </div>
+    //     </article>
+    //   `;
+    // })
   },
 
   showRoomDetails() {
