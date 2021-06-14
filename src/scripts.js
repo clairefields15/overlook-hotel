68f53e81f753e364df431a9120ad69cf8f58bc32
Claire Fields <clairefields15@gmail.com>
2021-06-13 20:57:57 -0600

    Refactor API calls, broke everything and fixed it

--- src/scripts.js
+++ src/scripts.js
@@ -1,66 +1,59 @@
-// imports
+// ***** ----- IMPORTS ----- ***** //
 import './css/index.scss';
 import './images/hotel-logo.png';
 import './images/hotel-image.jpg';
 import './images/hamburger.png';
 import './images/hamburger-close.png';
 import './images/1-bed-room.jpg';
-import domUpdates from './domUpdates'
+import domUpdates from './domUpdates';
 import apiCalls from './apiCalls';
 import Customer from './customer';
 import Booking from './booking';
 import Hotel from './hotel';
 import Room from './room';
 
-// query selectors
+// ***** ----- EXPORTS ----- ***** //
+export const errorTag = document.getElementById('errorTag');
+
+
+// ***** ----- DEPENDENTS ----- ***** //
+let dayjs = require('dayjs');
+const currentDate = dayjs(Date.now()).format('YYYY-MM-DD');
+let customer = null;
+let booking = null;
+let room = null;
+let hotel = null;
+let roomsData = null;
+let bookingsData = null;
+
+// ***** ----- QUERY SELECTORS ----- ***** //
 const hamburgerBtn = document.getElementById('hamburger');
 const mobileBookBtn = document.getElementById('mobileBook');
 const bookNowBtn = document.getElementById('bookNowBtn');
 const checkAvailBtn = document.getElementById('checkAvailBtn');
 const navBookBtn = document.getElementById('navBook');
-const bookRoomBtn = document.getElementById('bookRoomButton')
 const filteredResults = document.getElementById('filteredResults');
-const searchResultsPage = document.getElementById('searchResultsPage');
 const mobileViewProfileBtn = document.getElementById('mobileViewProfile');
 const modalOverlay = document.getElementById('modalOverlay');
 const arrivalDate = document.getElementById('arrivalDate');
-const searchAgainBtn = document.getElementById('searchAgain')
+const searchAgainBtn = document.getElementById('searchAgain');
 const selectRoomType = document.getElementById('selectRoomType');
 const roomTypeForm = document.getElementById('roomTypeForm');
-const goBackButton = document.getElementById('goBackButton')
 const selectedRoom = document.getElementById('selectedRoom');
 const changeDates = document.getElementById('changeDates');
-export const errorTag = document.getElementById('errorTag');
 const mobileLogInBtn = document.getElementById('mobileLogIn');
 const goHomeBtn = document.getElementById('mobileGoHome');
 const logInBtn = document.getElementById('logInBtn');
 const usernameField = document.getElementById('usernameField');
 const passwordField = document.getElementById('passwordField');
-const usernameError = document.getElementById('usernameError');
-const passwordError = document.getElementById('passwordError')
+const passwordError = document.getElementById('passwordError');
 
-// const mobileViewTripsBtn = document.getElementById('mobileViewTrips');
-// const navLogInBtn = document.getElementById('navLogIn');
-// const navTripsBtn = document.getElementById('navTrips');
-// const navProfileBtn = document.getElementById('navProfile');
-// const desktopNavSection = document.getElementById('desktopNav');
-// const landingPage = document.getElementById('landingPage');
-// const selectPage = document.getElementById('selectPage');
-// const bookingPage = document.getElementById('bookingPage');
-// const confirmationPage = document.getElementById('confirmationPage');
-  
-// variables
-let dayjs = require('dayjs')
-
-let customer, booking, room, hotel, currentDate;
-let customersData, roomsData, bookingsData
-
-// event listeners
-window.addEventListener('load', fetchHotelData);
-goHomeBtn.addEventListener('click', domUpdates.showLandingPage)
-mobileLogInBtn.addEventListener('click', domUpdates.showLogInView)
+// ***** ----- EVENT LISTENERS ----- ***** //
+window.addEventListener('load', apiCalls.fetchHotelData);
+goHomeBtn.addEventListener('click', domUpdates.showLandingPage);
+mobileLogInBtn.addEventListener('click', domUpdates.showLogInView);
 searchAgainBtn.addEventListener('click', domUpdates.showBookingView);
-changeDates.addEventListener('click', domUpdates.showBookingView)
+changeDates.addEventListener('click', domUpdates.showBookingView);
 hamburgerBtn.addEventListener('click', domUpdates.openMobileNav);
 mobileBookBtn.addEventListener('click', domUpdates.showBookingView);
 modalOverlay.addEventListener('click', domUpdates.hideOverlay);
@@ -71,109 +64,21 @@ checkAvailBtn.addEventListener('click', getRoomTypes);
 filteredResults.addEventListener('click', () => selectRoom(event));
 mobileViewProfileBtn.addEventListener('click', domUpdates.showUserProfile);
 selectRoomType.addEventListener('change', filterRoomsByType);
-selectedRoom.addEventListener('click', () => goBackToSearchResults(event))
+selectedRoom.addEventListener('click', () => viewOtherRooms(event));
 selectedRoom.addEventListener('click', () => bookRoom(event));
 logInBtn.addEventListener('click', () => logIn(event));
 
+// ***** ----- EVENT HANDLERS ----- ***** //
 
+// --- Page load --- //
 
-//event handlers and functions
-export function fetchHotelData() {
-  apiCalls.fetchHotelData();
+export function assignVariables(apiData) {
+  roomsData = apiData[0].rooms;
+  bookingsData = apiData[1].bookings;
 }
 
-export function assignVariables(data) {
-  roomsData = data[0].rooms;
-  bookingsData = data[1].bookings;
-}
-
-export function pageLoad() {
-  const allBookings = makeBookingInstances()
-  const allRooms = makeRoomInstances()
-  hotel = new Hotel(allBookings, allRooms)
-  getDate()
-}
-
-function makeRoomInstances() {
-  const allRooms = []
-  roomsData.forEach(roomObj => {
-    room = new Room (roomObj)
-    allRooms.push(room)
-  })
-  return allRooms
-}
-
-function makeBookingInstances() {
-  const allBookings = [];
-  bookingsData.forEach(bookingObj => {
-    booking = new Booking(bookingObj);
-    allBookings.push(booking);
-  });
-  return allBookings;
-}
-
-function getDate() {
-  currentDate = dayjs(Date.now()).format('YYYY-MM-DD');
-  arrivalDate.value = currentDate;
-  arrivalDate.min = currentDate;
-}
-
-function logIn(event) {
-  event.preventDefault()
-  const username = usernameField.value;
-  const password = passwordField.value;
-  checkEmptyFields(username, password);
-
-  try {
-    setUser(username, password)
-  } catch (error) {
-    passwordError.innerText = error;
-    hideError()
-  }
-}
-
-function hideError() {
-  setTimeout(() => passwordError.innerText = '', 2700)
-}
-
-function checkEmptyFields(username, password) {
-  if (username === '') {
-    usernameField.setAttribute('placeholder', '*username is required*')
-  }
-  if (password === '') {
-    passwordField.setAttribute('placeholder', '*password is required*');
-  }
-}
-
-function setUser(username, password) {
-  checkPassword(password)
-  checkUsername(username)
-  usernameField.value = '';
-  passwordField.value = '';
-  let id = username.slice(8);
-  if (id) {
-    apiCalls.fetchCustomerData(id);
-  } else {
-    domUpdates.showLogInError();
-    errorTag.innerText = 'User does not exist, please try again.';
-    throw new Error('User does not exist');
-  }
-}
-
-function checkPassword(password) {
-  if (password !== 'overlook2021') {
-    passwordField.value = ''
-    throw new Error('Incorrect password');
-  }
-}
-
-function checkUsername(username) {
-  if (!username.startsWith('username')) {
-    usernameField.value = ''
-    throw new Error ('Incorrect username')
-  }
-}
-
+// this runs in apicalls after a successful log in 
+// but maybe should also run after a successful booking? line 102 looks pretty similar
 export function instantiateUser(data) {
   customer = new Customer(data);
   const userBookings = hotel.getFullRoomInfoForBookings(customer);
@@ -185,8 +90,31 @@ export function instantiateUser(data) {
     userExpenses,
     currentDate
   );
+  domUpdates.showLandingPageAfterLogIn(customer);
+}
 
-  domUpdates.showLandingPageAfterLogIn(customer)
+export function pageLoad() {
+  const allBookings = makeBookingInstances();
+  const allRooms = makeRoomInstances();
+  hotel = new Hotel(allBookings, allRooms);
+  setDate();
+
+  // is all of this necessary? it runs after a successful booking
+  if (customer) {
+    const userBookings = hotel.getFullRoomInfoForBookings(customer);
+    const userExpenses = hotel.getUserExpenses(customer);
+    const sortedBookings = sortUserBookingsByDate(userBookings);
+    domUpdates.renderUserDashboard(
+      customer,
+      sortedBookings,
+      userExpenses,
+      currentDate
+    );
+    // maybe show dashboard instead of landing page?
+    setTimeout(function () {
+      domUpdates.showLandingPageAfterLogIn(customer);
+    }, 2000);
+  }
 }
 
 function sortUserBookingsByDate(bookings) {
@@ -195,31 +123,94 @@ function sortUserBookingsByDate(bookings) {
   });
 }
 
-//username: customer50 (where 50 is the ID of the user)
+function makeBookingInstances() {
+  const allBookings = [];
+  bookingsData.forEach(bookingObj => {
+    booking = new Booking(bookingObj);
+    allBookings.push(booking);
+  });
+  return allBookings;
+}
 
-// startsWith(customer)
-// makes sense to fetch all the customers to update the data source
+function makeRoomInstances() {
+  const allRooms = [];
+  roomsData.forEach(roomObj => {
+    room = new Room(roomObj);
+    allRooms.push(room);
+  });
+  return allRooms;
+}
 
-// checking the '50' against the users API, does a user with this ID exist
-// if the id exists, fetch a single customer
+function setDate() {
+  arrivalDate.value = currentDate;
+  arrivalDate.min = currentDate;
+}
 
-// on authentication you can fetch just the single customer
-// whatever number they put in, try to fetch from the API
-
-// try not to rely on users that are in storage to check the userIDs
-// you could see all of the users inside of the dev tools/browser
-// try to make a fetch request for a single user, based on the id
-// if 404 that user doesn't exist
-
-// within the manager class you might need to fetch all the customers but generally, not
+// --- User Log In  --- //
 
 function isCustomerLoggedIn() {
   if (customer) {
-    domUpdates.showBookingView()
+    domUpdates.showBookingView();
   } else if (!customer) {
-    domUpdates.showLogInView()
+    domUpdates.showLogInView();
   }
 }
+
+function logIn(event) {
+  event.preventDefault();
+  const username = usernameField.value;
+  const password = passwordField.value;
+  checkEmptyFields(username, password);
+
+  try {
+    setUser(username, password);
+  } catch (error) {
+    passwordError.innerText = error;
+    domUpdates.hideError();
+  }
+}
+
+function checkEmptyFields(username, password) {
+  if (username === '') {
+    usernameField.setAttribute('placeholder', '*username is required*');
+  }
+  if (password === '') {
+    passwordField.setAttribute('placeholder', '*password is required*');
+  }
+}
+
+function setUser(username, password) {
+  checkPassword(password);
+  checkUsername(username);
+  usernameField.value = '';
+  passwordField.value = '';
+
+  // this might be why username03 can log in as if username3
+  let id = username.slice(8);
+  if (id) {
+    apiCalls.fetchCustomer(id);
+  } else {
+    domUpdates.showLogInError();
+    errorTag.innerText = 'User does not exist, please try again.';
+    throw new Error('User does not exist');
+  }
+}
+
+function checkPassword(password) {
+  if (password !== 'overlook2021') {
+    passwordField.value = '';
+    throw new Error('Incorrect password');
+  }
+}
+
+function checkUsername(username) {
+  if (!username.startsWith('username')) {
+    usernameField.value = '';
+    throw new Error('Incorrect username');
+  }
+}
+
+// --- Search for Room  --- //
 
 function checkAvailability() {
   const input = dayjs(arrivalDate.value).format('YYYY/MM/DD');
@@ -230,18 +221,20 @@ function checkAvailability() {
     domUpdates.renderApology();
   } else {
     domUpdates.renderAvailableRooms(results);
-    domUpdates.renderYourDates(input)
+    domUpdates.renderYourDates(input);
   }
 }
 
+// --- Filter Rooms By Type  --- //
+
 function getRoomTypes() {
-  const results = hotel.getRoomTypes()
-  domUpdates.populateRoomTypeSelector(results)
+  const results = hotel.getRoomTypes();
+  domUpdates.populateRoomTypeSelector(results);
 }
 
 function filterRoomsByType() {
   const selection = roomTypeForm.value;
-  const results = hotel.filterAvailableRoomsByType(selection)
+  const results = hotel.filterAvailableRoomsByType(selection);
 
   if (results.length === 0) {
     domUpdates.renderApology();
@@ -250,31 +243,30 @@ function filterRoomsByType() {
   }
 }
 
+
+// --- Book a Room  --- //
+
 function selectRoom(event) {
   const date = dayjs(arrivalDate.value).format('YYYY/MM/DD');
-  const roomNumber = parseInt(event.target.closest('.room-card').id)
-  const roomDetails = hotel.returnRoomDetails(roomNumber)
-  domUpdates.showRoomDetails()
+  const roomNumber = parseInt(event.target.closest('.room-card').id);
+  const roomDetails = hotel.returnRoomDetails(roomNumber);
+  domUpdates.showRoomDetails();
   domUpdates.renderSelectedRoom(roomDetails, date);
-}
-
-function goBackToSearchResults(event) {
-  if (event.target.id === 'goBackButton') {
-    domUpdates.showSearchResultsPage();
-  }
 }
 
 function bookRoom(event) {
   if (event.target.classList.contains('book-room-btn')) {
-    const user = customer
+    const user = customer;
     const date = dayjs(arrivalDate.value).format('YYYY/MM/DD');
-    const roomNumber = parseInt(event.target.id)
+    const roomNumber = parseInt(event.target.id);
 
-    apiCalls.bookRoom(user, date, roomNumber)
+    apiCalls.bookRoom(user, date, roomNumber);
   }
 }
 
-
-
-
+function viewOtherRooms(event) {
+  if (event.target.id === 'goBackButton') {
+    domUpdates.showSearchResultsPage();
+  }
+}
 
